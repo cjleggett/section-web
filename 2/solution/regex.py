956f21ad059aeb77b2588ff3161c3961ca194b1c
Connor Leggett <cleggett@Connors-MacBook-Pro.local>
2020-07-02 16:53:32 +0100

    Added No-No's regex for piglatinify

--- 2/solution/regex.py
+++ 2/solution/regex.py
@@ -64,34 +64,18 @@ def piglatinify(st):
     Takes in a string, and returns the pig-latin translation of
     that string.
     Note: You may assume there will be no punctuation for now
-    HINT: You may not end up using regular expressions for this one...
 
     "hello, connor" -> "ello-hay, onnor-cay
     """
-    VOWELS = [letter for letter in "aeiou"]
-    CONS = [letter for letter in "qwrtyplkjhgfdszxcvbnm"]
 
-    # Make string lowercase, and return itsself if it is the empty string
-    st = st.lower()
-    if st == "":
-        return ""
-    
-    # Create a new string to return
-    new_st = ""
-    
-    # Change each word to pig latin
-    for word in st.split(" "):
-        if word[0] in VOWELS:
-            new_st += f"{word}-way "
-        elif word[1] in CONS:
-            new_st += f"{word[2:]}-{word[0:2]}ay "
-        else:
-            new_st += f"{word[1:]}-{word[0]}ay "
+    # Thanks No-No (@Steryl on GitHub) for this solution:
 
-    # removing trailing space
-    new_st = new_st[:-1]
-
-    return new_st
+    # (\s|^)        matches any whitespace or start of the line
+    # [aeiou]\w+    matches any vowel followed by word-characters
+    # [^aeiou]+     matches any non-vowel of one or more
+    st = re.sub(r"((\s|^)[aeiou]\w+)", r"\1-way", st)
+    st = re.sub(r"(\s|^)([^aeiuo]+)(\w+)", r"\1\3-\2ay", st)
+    return st
 
         
 
